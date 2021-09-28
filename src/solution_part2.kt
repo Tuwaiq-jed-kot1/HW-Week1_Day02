a269fcd78d2063c560071f389c7162c334ac29ea
asmaa <Asma.Alghamdi.jed.k1@tuwaiq.edu.sa>
2021-09-29 00:22:16 +0300

    day-2 assignment . -Asmaa

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,16 @@
 fun main(){
- print("I love Kotlin!!!!")
+ // two players with different points to cover the two cases
+ val healthpointPlayer1 = 40
+ val healthpointPlayer2=90
+ println("the first player result: ${heathPoint(healthpointPlayer1)}")
+ println("the second player result: ${heathPoint(healthpointPlayer2)}")
+}
+
+fun heathPoint(healthpoint:Int):String{
+ return when (healthpoint) {
+  in 0..49 -> "the player's health is awful"
+  in 50..100 -> "the player's health is fine"
+  else -> "invalid input"
+ }
+
 }
