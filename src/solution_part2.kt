0458edbc07b5a656cafb2a6ee3a4a60c10948b03
rubaomar <Rr094794003>
2021-10-02 02:27:12 +0300

    :)

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,24 @@
-fun main(){
- print("I love Kotlin!!!!")
+// Q1 -> Integr
+// Q2 -> you are adult
+
+// Q3
+fun main() {
+
+    val player1 = " Salem "
+    val healthPoint1 = 30
+    val player2 = " Muhammad "
+    val healthPoint2 = 70
+
+    if (healthPoint1 < 90 && healthPoint1 > 50 && healthPoint1 <= 100 && healthPoint1 > 0) {
+        println(" $player1's health is fine ")
+    }else if ( healthPoint1 <50)
+        println(" $player1's health is awful " )
+    else println(" error: the range is from 0 to 100")
+
+    if (healthPoint2 < 90 && healthPoint2 > 50 && healthPoint2 <= 100 && healthPoint2 > 0) {
+        println(" $player2's health is fine ")
+    }else if ( healthPoint2 <50)
+        println(" $player2's health is awful " )
+    else println(" error: the range is from 0 to 100")
+
 }
