aaa342f8ce37b3c41ae2992889f1f2abc6b1076f
faisalha98 <fisal.alharthi.jed.k1@tuwaiq.edu.sa>
2021-10-01 16:01:17 +0300

    sory i forgit it

--- solution.kt
+++ solution.kt
@@ -11,3 +11,20 @@ if(age < 18 ) {
     print("you are adult")
 }
 /////////////////////////////////
+
+// part 2
+// two players with different points to cover the two cases
+fun main (){
+val healthpointPlayer1 = 40
+val healthpointPlayer2=90
+println(" first player result: ${heathPoint(healthpointPlayer1)}")
+println(" second player result: ${heathPoint(healthpointPlayer2)}")
+}
+
+fun heathPoint(healthpoint:Int):String{
+    return when (healthpoint) {
+        in 0..49 -> "the player's health is awful"
+        in 50..100 -> "the player's health is fine"
+        else -> "invalid input"
+    }
+}
