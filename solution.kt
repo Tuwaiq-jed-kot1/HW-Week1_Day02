49ff91e0b4c5492972a7f898acc129d9e5277c78
faisalha98 <fisal.alharthi.jed.k1@tuwaiq.edu.sa>
2021-10-01 15:57:35 +0300

    sory i forgit it

---
+++ solution.kt
@@ -0,0 +1,13 @@
+## Part I - short answer questions.
+---
+### what is the type of this Variable `var age:int = 19`: ---> Integer
+###
+###
+### What is the output of this code:  "you are adult"
+###  `var age:int = 19`
+if(age < 18 ) {
+    print("you are not adult")
+}else {
+    print("you are adult")
+}
+/////////////////////////////////
