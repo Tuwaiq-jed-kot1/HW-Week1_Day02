e572465be8767c14114f8638592237783a6b32ee
Renad Omar <reomqu@gmail.com>
2021-10-23 02:31:38 +0300

    Thank you

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,17 @@
-fun main(){
- print("I love Kotlin!!!!")
+fun main() {
+ //part||
+ var player1: Int = 40
+ var player2: Int = -1
+
+
+ fun checkHealth(points: Int) {
+  when (points) {
+   in 50..100 -> println("the player's health is fine")
+   in 0..49 -> println("the player's health is awful")
+   else -> println("out of range")
+  }
+ }
+
+ checkHealth(player1)
+ checkHealth(player2)
 }
