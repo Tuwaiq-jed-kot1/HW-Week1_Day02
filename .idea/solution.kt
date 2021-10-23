ee2eab6cc1ce5e460d700f60f539ae246feb35f8
SuzanneSaleem1 <Suzan.Alluhaybi.jed.k1@tuwaiq.edu.sa>
2021-10-23 17:23:58 +0300

    week 2, day 2

---
+++ .idea/solution.kt
@@ -0,0 +1,18 @@
+fun main(){
+    val firstPlayerName = "Ahmed"
+    var firstHP = 93
+    val secoendPlayerName = "Ali"
+    var secoendHP = 50
+
+    if (firstHP  > 90 ) {
+        println(firstPlayerName + "  health is fine")
+    } else {
+        println(firstPlayerName + "  health is awful")
+    }
+
+    if (secoendHP > 90) {
+        println(secoendPlayerName + "  health is fine")
+    } else {
+        println(secoendPlayerName + "  health is awful")
+    }
+}
